b0344eba11e7b9d4fd3efa610abeb1f7baefd13f
IgorMal17 <IgorMal17>
2025-04-08 09:54:00 +0500

    Подключение к базе данных(Npgsql.EntityFrameworkCore.PostgreSQL и Microsoft.EntityFrameworkCore.Tools). Переименованы навигационные свойства.

---
+++ Models/TitlesGenre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AppAnimeTitles.Models;
+
+public partial class TitlesGenre
+{
+    public int Id { get; set; }
+
+    public short IdGenres { get; set; }
+
+    public int IdAnime { get; set; }
+
+    //Навигационные свойства
+    public virtual AnimeTitle AnimeTitle { get; set; } = null!;
+
+    public virtual Genre Genre { get; set; } = null!;
+}
---
+++ Models/Type.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AppAnimeTitles.Models;
+
+public partial class Type
+{
+    public short Id { get; set; }
+
+    public string TypeName { get; set; } = null!;
+
+    //Навигационные свойства
+    public virtual ICollection<AnimeTitle> AnimeTitles { get; set; } = new List<AnimeTitle>();
+}
